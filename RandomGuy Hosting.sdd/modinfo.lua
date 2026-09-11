704af21c55001efe2b6faba86ab75a8f631a9842
RandomGuyJunior <42807509+RandomGuyJunior@users.noreply.github.com>
2026-09-11 15:44:09 +0200

    use $VERSION variable

--- RandomGuy Hosting.sdd/modinfo.lua
+++ RandomGuy Hosting.sdd/modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = "RandomGuy Hosting",
 	description = "RandomGuy BAR hosting mutator",
 	shortname = "RGHOST",
-	version = "v1",
+	version = "$VERSION",
 	mutator = "RandomGuy",
 	game = "Beyond All Reason",
 	shortGame = "BYAR",
