daf46ed3b5da64064d124fa482ba6a25d381a507
RandomGuyJunior <42807509+RandomGuyJunior@users.noreply.github.com>
2026-09-20 11:40:15 +0200

    use latest BAR version

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
 	name = "RandomGuy Hosting",
 	description = "RandomGuy BAR hosting mutator",
 	shortname = "RGHOST",
-	version = "v1",
+	version = "$VERSION",
 	mutator = "RandomGuy",
 	game = "Beyond All Reason",
 	shortGame = "BYAR",
 	modtype = 1,
 
 	depend = {
-		"Beyond All Reason test-31251-0ceadc7",
+		"rapid://byar:test",
 	},
 }
