9765dcba5e85316279a2f8389471437d866400a0
RandomGuyJunior <yquestmastery@gmail.com>
2026-09-26 11:32:55 +0200

    Tweaking it again

--- gamedata/afus_supremacy.lua
+++ gamedata/afus_supremacy.lua
@@ -41,7 +41,7 @@ local function makeLauncher(model)
 		tolerance = 10000,
 		tracks = true,
 		turnrate = 18000,
-		weapontimer = 3,
+		weapontimer = 2,
 		weaponacceleration = 600,
 		weapontype = "StarburstLauncher",
 		weaponvelocity = 2200,
